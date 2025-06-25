60ca1808a8f62b0ffde18f6274cab7d4ad59f6b0
Dieote <gomezmdiego97@gmail.com>
2025-06-25 18:13:57 +0200

    Ejercicio calculadora
    Mejoras

--- Ejercicios/Calculadora/js/app.js
+++ Ejercicios/Calculadora/js/app.js
@@ -10,3 +10,39 @@ function sumar(){
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
     console.log(`Resultado: ${resultado}`);
 }
+function restar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+function dividir(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+function multiplicar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+
+function limpiar(){
+    document.getElementById('operandoA').value = '';
+    document.getElementById('operandoB').value = '';
+    document.getElementById('resultado').innerHTML = '';
+}
