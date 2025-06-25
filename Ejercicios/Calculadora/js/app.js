f2c96173b129305eaaa7c7972174061938f17407
Dieote <gomezmdiego97@gmail.com>
2025-06-25 20:17:50 +0200

    Operaciones
    Mejora de codigo

--- Ejercicios/Calculadora/js/app.js
+++ Ejercicios/Calculadora/js/app.js
@@ -4,39 +4,39 @@ function sumar(){
     const forma = document.getElementById('forma');
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
-    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
-    if(isNaN(resultado))
-        resultado = 'La operación no incluye números';
-    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
-    console.log(`Resultado: ${resultado}`);
+    let resultado = parseFloat(operandoA.value) + parseFloat(operandoB.value);
+    mostrarResultado(resultado);
 }
 function restar(){
     const forma = document.getElementById('forma');
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
-    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
-    if(isNaN(resultado))
-        resultado = 'La operación no incluye números';
-    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
-    console.log(`Resultado: ${resultado}`);
+    let resultado = parseFloat(operandoA.value) - parseFloat(operandoB.value);
+    mostrarResultado(resultado);
 }
 function dividir(){
     const forma = document.getElementById('forma');
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
-    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
-    if(isNaN(resultado))
-        resultado = 'La operación no incluye números';
-    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
-    console.log(`Resultado: ${resultado}`);
+    let resultado = parseFloat(operandoA.value) / parseFloat(operandoB.value);
+    if (operandoB === 0) {
+        mostrarResultado('No se puede dividir entre cero');
+    } else {
+        mostrarResultado(resultado);
+    }
 }
 function multiplicar(){
     const forma = document.getElementById('forma');
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
-    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
-    if(isNaN(resultado))
+    let resultado = parseFloat(operandoA.value) * parseFloat(operandoB.value);
+    mostrarResultado(resultado);
+}
+
+function mostrarResultado(resultado){
+    if(isNaN(resultado)){
         resultado = 'La operación no incluye números';
+    }
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
     console.log(`Resultado: ${resultado}`);
 }
