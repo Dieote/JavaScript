57d2d7da65fafc98de4a723fd55b3fec64f71748
Dieote <gomezmdiego97@gmail.com>
2025-09-03 17:19:07 +0200

    Error
    Error al querer instalar una dependencia no coincide con las de la creacion de proyecto.

--- calendarioCSemana/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './appCalendario/app.config';
-import { AppCalendario } from './appCalendario/appCalendario';
-
-bootstrapApplication(AppCalendario, appConfig)
-  .catch((err) => console.error(err));
