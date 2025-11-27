44083166e5d170ad3c41d548b72ce51f0518387d
Dieote <gomezmdiego97@gmail.com>
2025-11-27 16:59:00 +0100

    logica del juego
    se separa funcionalidad del .script.js

---
+++ BlackJackGame/juego.js
@@ -0,0 +1,61 @@
+class Juego {
+    constructor(nombreJugador) {
+        this.mazo = new Mazo();
+        this.banca = new Jugador('Banca');
+        this.jugador = new Jugador(nombreJugador);
+    }
+    inicioDeJuego() {
+        this.banca.resetear();
+        this.jugador.resetear();
+        this.mazo.crearMazo();
+    }
+
+    juegaLaBanca(sePlanta, sigueRobando) {
+        let turnoBanca = () => {
+            this.banca.recibirCarta(this.mazo.robarCarta());
+
+    if(this.banca.puntos >= 21) {
+        sePlanta("banca-se-paso");
+        return;
+    }
+
+    if (this.banca.puntos >= 17) {
+        sigueRobando();
+        return;
+    }
+        setTimeout(turnoBanca, 1000);
+    };
+    turnoBanca();
+}
+
+    botonPedir() {
+        this.jugador.recibirCarta(this.mazo.robarCarta());
+    }
+
+    botonPlantarse() {
+        decidirGanador();
+    }
+
+    decidirGanador() {
+        let bancaPuntos = this.banca.puntos;
+        let jugadorPuntos = this.jugador.puntos;
+
+        if (jugadorPuntos > 21) return "jugador-se-paso";
+        if (bancaPuntos > 21) return "banca-se-paso";
+        if (bancaPuntos > jugadorPuntos) return "gana-banca";
+        if (jugadorPuntos > bancaPuntos) return "gana-jugador";
+        return "empate";
+ /*       if (bancaPuntos > jugadorPuntos) {
+            finalizarJuego('La banca gana.');
+        } else if (bancaPuntos < jugadorPuntos) {
+            finalizarJuego('¡Ganaste! La banca pierde.');
+        } else if (jugadorPuntos > 21) {
+            finalizarJuego('La banca gana! TE PASASTE');
+        } else if (bancaPuntos > 21) {
+            finalizarJuego('¡Ganaste! La banca se paso');
+        } else {
+            finalizarJuego('Empate.');
+        } */
+    }
+}
+    
--- BlackJackGame/script.js
+++ BlackJackGame/script.js
@@ -1,24 +1,4 @@
-let nombreJugador = '';
-let mazo = [];
-let banca = { cartas: [], puntos: 0 };
-let jugador = { cartas: [], puntos: 0 };
-
-// DOM
-const bancaCartasDiv = querySelector('#bancaCards');
-const jugadorCartasDiv = querySelector('#jugadorCards');
-const bancaPuntosSpan = querySelector('#bancaScore');
-const jugadorPuntosSpan = querySelector('#jugadorScore');
-const botonPedir = querySelector('#botonPedir');
-const botonPlantarse = querySelector('#botonPlantarse');
-const botonReiniciar = querySelector('#botonReiniciar');
-
-const bancaPoints = querySelector('#bancaPoints');
-const userPoints = querySelector('#userPoints');
-const bancaTotalCards = querySelector('#bancaTotalCards');
-const userTotalCards = querySelector('#userTotalCards');
-const bancaStatus = querySelector('#bancaStatus');
-const userStatus = querySelector('#userStatus');
-const userName = querySelector('#userName');
+let juego = null;
 
 window.addEventListener('DOMContentLoaded', () => {
     pedirNombreJugador();
@@ -44,106 +24,62 @@ function pedirNombreJugador() {
         nombreJugador = resultado.value;
         document.getElementById('jugadorNameValue').textContent = nombreJugador;
         userName.textContent = nombreJugador;
-        iniciarJuego();
+        iniciarJuego(nombreJugador);
     });
 }
 
-// MAzo y cartas
-function crearMazo() {
-    const palos = [' ♠', ' ♣', ' ♦', ' ♥'];
-    const valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-    mazo = [];  
-    for (let valor of valores) {
-        for (let palo of palos) {
-            mazo.push({ valor, palo });
-        }
+function iniciarJuego(nombreJugador) {
+    juego = new Juego(nombreJugador);
+    juego.inicioDeJuego();
+    vista.bancaStatus.textContent = 'Jugando..';
+    vista.userStatus.textContent = 'Esperando..';
+    
+    vista.actualizarVista(juego);
+    juego.juegaLaBanca(bancaFin, comenzarTurnoJugador);
+}
+
+function bancaFin(piensa) {
+    if (piensa === "banca-se-paso") {
+        finalizarJuego('¡Ganaste! La banca se paso');
     }
-    mazo.sort(() => Math.random() - 0.5); // mezcla
 }
 
-function repartirCartas(recibeJugador) {
-    const carta = mazo.pop(); //saca del mazo la ultma carta
-    recibeJugador.cartas.push(carta);
-    recibeJugador.puntos = calcularLosPuntos(recibeJugador.cartas);
-    actualizarVista();
-}
-
-function calcularLosPuntos(cartas) {
-    let total = 0;
-    for (let carta of cartas) {
-        if (carta.valor === 'A') total += 1;
-        else if (['J', 'Q', 'K'].includes(carta.valor)) total += 10;
-        else total += parseInt(carta.valor);
-    }
-    return total;
-}
-
-function actualizarVista() {
-    bancaCartasDiv.innerHTML = banca.cartas.map(c => c.valor + c.palo).join(' ');
-    jugadorCartasDiv.innerHTML = jugador.cartas.map(c => c.valor + c.palo).join(' ');
-}
-
-function iniciarJuego() {
-    crearMazo();
-   banca = { cartas: [], puntos: 0 };
-    jugador = { cartas: [], puntos: 0 };
-    actualizarVista();
-    botonPedir.disabled = true;
-    botonPlantarse.disabled = true;
-    bancaStatus.textContent = 'Jugando..';
-    userStatus.textContent = 'Esperando..';
-    juegaLaBanca();
-}
-
-function juegaLaBanca() {
-    //con set interval puedo ver como la banca saca cartas de forma pausada
-    // crea una sensación real de que esta “pensando.
-    const bancaIntervalo = setInterval(() => {
-        repartirCartas(banca);
-        if (banca.puntos >= 17) {
-            clearInterval(bancaIntervalo);
-            if (banca.puntos > 21) {
-                bancaStatus.textContent = 'Se pasó!';
-                finalizarJuego('¡Ganaste! La banca se pasó.');
-            } else {
-                bancaStatus.textContent = 'Plantada';
-                comenzarTurnoJugador();
-            }
-        }
-    }, 1000);
-}
-
-// Turno Jugador
 function comenzarTurnoJugador() {
     botonPedir.disabled = false;
     botonPlantarse.disabled = false;
-    userStatus.textContent = 'Jugando..';
+    vista.bancaStatus.textContent = 'Plantada';
+    vista.userStatus.textContent = 'Tu turno';
 }
 
-botonPedir.addEventListener('click', () => {
-    // usar la función definida `repartirCartas`
-    repartirCartas(jugador);
-    if (jugador.puntos > 21) {
-        userStatus.textContent = 'Se pasó';
-        finalizarJuego('Te has pasado. Pierdes.');
+botonPedir.onclick = () => {
+    juego.botonPedir();
+    vista.actualizarVista(juego);
+
+    if (juego.jugador.puntos > 21) {
+        finalizarJuego('La banca gana! TE PASASTE');
     }
-});
+};
+botonPlantarse.onclick = () => {
+    let resultado = juego.botonPlantarse();
+    let mensajes = {
+        "gana-banca": "La banca gana.",
+        "gana-jugador": "¡Has ganado!",
+        "empate": "Empate.",
+        "jugador-se-paso": "Te pasaste. Pierdes.",
+        "banca-se-paso": "¡La banca se pasó!"
+    };
+    finalizarJuego(mensajes[resultado]);
+}
 
-botonPlantarse.addEventListener('click', () => {
-    userStatus.textContent = 'Plantado';
-    decidirGanador();
-});
-
-botonReiniciar.addEventListener('click', () => {
-    location.reload();
-});
-
+botonReiniciar.onclick = () => {
+    iniciarJuego(nombreJugador);
+}
 
 
 function finalizarJuego(mensaje) {
     botonPedir.disabled = true;
     botonPlantarse.disabled = true;
-    userStatus.textContent = 'Finalizado';
+    vista.userStatus.textContent = 'Finalizado';
 
     Swal.fire({
         title: 'Resultado',
