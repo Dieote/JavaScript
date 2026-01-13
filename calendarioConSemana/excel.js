8be4496c9f05626e5f8f5f0cad9f861afe762b8d
Dieote <gomezmdiego97@gmail.com>
2026-01-13 11:45:34 +0100

    logica de busqueda
    hasta aqui es funcional la busqueda de una fila y columna, la cual devuelve el valor como estado (libre,vacaciones,trabajado)

--- calendarioConSemana/excel.js
+++ calendarioConSemana/excel.js
@@ -34,5 +34,184 @@ function procesarWorkbook(workbook) {
     
     console.log(`Total de filas: ${totalFilas} Total de columnas: ${totalColumnas}`);
     
+    //detectarTrabajadores(worksheet);
+    //detectarSemanas(worksheet);
+    //detectarDias(worksheet);
+
+    /* obtenerEstadoCelda(worksheet, 23, 33); // GRIS
+    obtenerEstadoCelda(worksheet, 24, 33); // ROJO CLARO
+    obtenerEstadoCelda(worksheet, 31, 33); // cELESTE
+    obtenerEstadoCelda(worksheet, 228, 33); // verdeFuerte
+    obtenerEstadoCelda(worksheet, 375, 33); // rojoFuerte */
 }
 
+function detectarTrabajadores(worksheet) {
+  const FILA_NOMBRES = 1;
+  const COLUMNA_INICIO = 3;   // C
+  const COLUMNA_FIN = 61;     // BI
+
+  const trabajadores = [];
+
+  for (let col = COLUMNA_INICIO; col <= COLUMNA_FIN; col++) {
+    const cell = worksheet.getRow(FILA_NOMBRES).getCell(col);
+    const valor = cell.value;
+
+    if (typeof valor === 'string' && valor.trim() !== '') {
+      trabajadores.push({
+        nombre: valor.trim(),
+        columna: col
+      });
+    }
+  }
+
+  console.log('Trabajadores detectados:', trabajadores);
+  return trabajadores;
+}
+
+function detectarSemanas(worksheet) {
+  const COLUMNA_SEMANA = 1; // A
+  const FILA_INICIO = 9;
+
+  const semanas = [];
+
+  for (let row = FILA_INICIO; row <= worksheet.rowCount; row++) {
+    const cell = worksheet.getRow(row).getCell(COLUMNA_SEMANA);
+    const valor = cell.value;
+
+    if (typeof valor === 'string' && valor.toLowerCase().includes('semana')) {
+      const numero = parseInt(valor.replace(/\D/g, ''), 10); // remover todo menos dígitos
+
+      semanas.push({
+        semana: numero,
+        filaInicio: row,
+        filaFin: row + 6
+      });
+    }
+  }
+
+  console.log('Semanas detectadas:', semanas);
+  return semanas;
+}
+
+function detectarDias(worksheet) {
+  const COLUMNA_DIA = 2; // B
+  const FILA_INICIO = 9;
+
+  const diasPorFila = {};
+
+  for (let row = FILA_INICIO; row <= worksheet.rowCount; row++) {
+    const cell = worksheet.getRow(row).getCell(COLUMNA_DIA);
+    const valor = cell.value;
+
+    if (typeof valor === 'string' && valor.trim() !== '') {
+      diasPorFila[row] = valor.trim().toUpperCase();
+    }
+  }
+
+  console.log('Días detectados por fila:', diasPorFila);
+  return diasPorFila;
+}
+
+//subir a git hasta aca
+//function detectarTurnos(worksheet) {}
+
+//Logica colores + valores
+
+function leerCelda(worksheet, fila, col) {
+    const cell = worksheet.getRow(fila).getCell(col);
+    const valor = cell.value;
+
+    let colorInfo = null;
+
+  if (cell.fill && cell.fill.fgColor) {
+    const fg = cell.fill.fgColor;
+
+    colorInfo = {
+      argb: fg.argb || null,
+      theme: fg.theme ?? null,
+      tint: fg.tint ?? null,
+      indexed: fg.indexed ?? null
+    };
+  }
+
+  /* console.log(
+    `DEBUG CELDA fila ${fila}, col ${col}`,
+    {
+      valor,
+      fill: cell.fill,
+      colorInfo
+    }
+  ); */
+
+    return { valor, colorInfo };
+
+}
+
+function hexCambiaRgb(argb) {
+    if (!argb) return null;
+    const hex = argb.substring(2); // Extraer el canal
+    const r = parseInt(hex.slice(0, 2), 16);
+    const g = parseInt(hex.slice(2, 4), 16);
+    const b = parseInt(hex.slice(4, 6), 16);
+    return { r, g, b };
+}
+
+function clasificarEstado(valor, colorInfo) {
+
+  if (valor === 1 || valor === "1") return "TRABAJADO";
+  if (valor === 0 || valor === "0") return "VACACIONES";
+
+  // Si no hay valor, analizamos color
+  if (!colorInfo) return "DESCONOCIDO";
+
+  const { argb, theme, tint } = colorInfo;
+  
+  //colores
+  if (theme === 0 && tint !== null && tint < 0) {
+    return "PUENTE"; // gris
+  }
+
+   if (theme === 5 && tint !== null && tint > 0) {
+    return "FESTIVO"; // rojo claro - rosado
+  }
+
+  if (argb) {
+    const rgb = hexCambiaRgb(argb);
+
+    const margenColor = 20; // Margen para comparar colores
+    const colores = {
+      amarillo: { r: 255, g: 255, b: 153 },
+      //rosado: { r: 248, g: 203, b: 173 },
+      //rojoClaro: { r: 244, g: 176, b: 132 },
+      rojoFuerte: { r: 255, g: 0, b: 0 },
+      celeste: { r: 102, g: 255, b: 204 },
+      //gris: { r: 191, g: 191, b: 191 },
+      verdeFuerte: { r: 0, g: 176, b: 80 }
+    }; 
+    
+    function colorMatch(c1, c2) {
+      return Math.abs(c1.r - c2.r) <= margenColor &&
+            Math.abs(c1.g - c2.g) <= margenColor &&
+            Math.abs(c1.b - c2.b) <= margenColor;
+    }
+    
+      if (colorMatch(rgb, colores.amarillo)) return "TRABAJADO";
+      //if (colorMatch(colorInfo, colores.rosado)) return "FESTIVO";
+      //if (colorMatch(colorInfo, colores.rojoClaro)) return "FESTIVO";
+      if (colorMatch(rgb, colores.celeste)) return "LIBRE";
+      //if (colorMatch(colorInfo, colores.gris)) return "PUENTE";
+      if (colorMatch(rgb, colores.rojoFuerte)) return "VACACIONES";
+      if (colorMatch(rgb, colores.verdeFuerte)) return "VACACIONES";
+      return "DESCONOCIDO";
+    }    
+    return "DESCONOCIDO";
+}
+
+function obtenerEstadoCelda(worksheet, fila, col) {
+    const { valor, colorInfo } = leerCelda(worksheet, fila, col);
+    const estado =  clasificarEstado(valor, colorInfo);
+    console.log( `CELDA fila ${fila}, col ${col} → ESTADO: ${estado}`,
+    { valor, colorInfo });
+    
+    return estado;
+}
